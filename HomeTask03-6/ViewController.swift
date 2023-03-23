23a5317f6e18936f5aca8296908afd6086825600
Sonun <usubalievasonun@gmail.com>
2023-03-23 22:27:59 +0600

    Initial Commit

---
+++ HomeTask03-6/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HomeTask03-6
+//
+//  Created by Sonun on 23/3/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
